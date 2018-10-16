74a364f9314995ff47e45a4f8bdedf2d37f8365d
Francois ANDRE <francois.andre@carnavello.fr>
2018-10-16 23:15:32 +0200

    Ajout méthode suppression

---
+++ src/main/java/fr/cnrs/ciham/meditha/meditha/aws/MetadataDeleteFunctionHandler.java
@@ -0,0 +1,108 @@
+package fr.cnrs.ciham.meditha.meditha.aws;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+
+import org.apache.http.HttpStatus;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+
+public class MetadataDeleteFunctionHandler implements RequestStreamHandler, Constants {
+
+	JSONParser parser = new JSONParser();
+	@Override
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException
+	{
+		boolean result = false;
+		MongoClientURI uri = new MongoClientURI(URL);
+		String json = "";
+		JSONObject responseJson = new JSONObject();
+		String responseCode = ""+HttpStatus.SC_OK;
+		JSONObject responseBody = new JSONObject();
+		String message="";
+		LambdaLogger logger = context.getLogger();
+		logger.log("Suppression d'une fiche");
+
+
+		try (MongoClient mongoClient = new MongoClient(uri)) {
+
+			String uuid = "";
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+			JSONObject event = (JSONObject) parser.parse(reader);
+			if (event.get("pathParameters") != null) {
+				JSONObject pathParameters = (JSONObject)event.get("pathParameters");
+				if ( pathParameters.get("uuid") != null) {
+					  uuid = (String) pathParameters.get("uuid");
+					  logger.log("uuid "+uuid);
+				}
+				else {
+					responseCode = ""+HttpStatus.SC_BAD_REQUEST;
+					message ="Missing parameter uuid";
+					
+				}
+			}	
+
+			MongoDatabase database = mongoClient.getDatabase(MONGO_DATABASE);
+			MongoCollection<Document> collection = database.getCollection(MONGO_METADATA_COLLECTION);
+			
+			BasicDBObject filter = new BasicDBObject();
+			filter.put("_id", uuid);
+			DeleteResult deleteOne = collection.deleteOne(filter);
+			if (deleteOne.getDeletedCount() != 1) {
+				responseJson.put("statusCode", HttpStatus.SC_INTERNAL_SERVER_ERROR);
+				responseCode = ""+HttpStatus.SC_INTERNAL_SERVER_ERROR;
+				message ="incorrect deleted count - "+deleteOne.getDeletedCount()+ " - "+uuid;
+				responseJson.put("exception", message);
+			} else {
+				result = true;
+				responseJson.put("statusCode", HttpStatus.SC_OK);
+				message = "success";
+			}
+			
+			
+			
+		}
+		catch (Exception e) {
+			responseJson.put("statusCode", HttpStatus.SC_INTERNAL_SERVER_ERROR);
+			responseJson.put("exception", e);
+			message =e.getMessage();
+		}
+
+		
+		
+		JSONObject headerJson = new JSONObject();
+		headerJson.put("Access-Control-Allow-Origin", "*");
+		responseJson.put("isBase64Encoded", false);
+		responseJson.put("statusCode", responseCode);
+		responseJson.put("headers", headerJson);
+		if (result) {
+			responseBody.put("result", "0K");
+		}
+		else {
+			responseBody.put("result", "KO: "+message);
+		}
+		responseJson.put("body", responseBody.toString());  
+		OutputStreamWriter writer = new OutputStreamWriter(outputStream, Charset.defaultCharset());
+		writer.write(responseJson.toJSONString());  
+		writer.close();
+	}
+}
+
