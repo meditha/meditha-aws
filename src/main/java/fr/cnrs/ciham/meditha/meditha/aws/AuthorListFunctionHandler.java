eb92d17bbd4db19c57f558234e788c745916eea2
Francois ANDRE <francois.andre@carnavello.fr>
2018-10-26 00:13:30 +0200

    Metadata save

---
+++ src/main/java/fr/cnrs/ciham/meditha/meditha/aws/AuthorListFunctionHandler.java
@@ -0,0 +1,72 @@
+package fr.cnrs.ciham.meditha.meditha.aws;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.bson.Document;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.w3c.dom.stylesheets.DocumentStyle;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+
+public class AuthorListFunctionHandler implements RequestStreamHandler, Constants {
+
+	JSONParser parser = new JSONParser();
+	@Override
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException
+	{
+
+		MongoClientURI uri = new MongoClientURI(URL);
+		String json = "";
+		JSONObject responseJson = new JSONObject();
+		String responseCode = "200";
+		responseJson.put("statusCode", responseCode);
+		JSONObject responseBody = new JSONObject();
+		try (MongoClient mongoClient = new MongoClient(uri)) {
+		MongoDatabase database = mongoClient.getDatabase(MONGO_DATABASE);
+		MongoCollection<Document> collection = database.getCollection(MONGO_AUTHOR_COLLECTION);
+		FindIterable<Document> documents = collection.find();
+		json = StreamSupport.stream(documents.spliterator(), false)
+		        .map(Document::toJson)
+		        .collect(Collectors.joining(", ", "[", "]"));
+		
+		JSONParser parser = new JSONParser();
+		JSONArray jsonObject = (JSONArray) parser.parse(json);
+    	responseBody.put("detail", jsonObject);
+    	
+		}
+		catch (Exception e) {
+			responseJson.put("statusCode", "500");
+			responseJson.put("exception", e);
+		}
+		
+		
+		JSONObject headerJson = new JSONObject();
+		headerJson.put("Access-Control-Allow-Origin", "*");
+		responseJson.put("isBase64Encoded", false);
+		responseJson.put("statusCode", responseCode);
+		responseJson.put("headers", headerJson);
+		responseJson.put("body", responseBody.toString());  
+		OutputStreamWriter writer = new OutputStreamWriter(outputStream, Charset.defaultCharset());
+		writer.write(responseJson.toJSONString());  
+		writer.close();
+	}
+}
+
--- src/main/java/fr/cnrs/ciham/meditha/meditha/aws/Constants.java
+++ src/main/java/fr/cnrs/ciham/meditha/meditha/aws/Constants.java
@@ -5,5 +5,6 @@ public interface Constants {
 	public final String URL = "mongodb+srv://meditaAdmin:medithaAdmin@meditha-ly5tt.mongodb.net/test?retryWrites=true;";
 	public final String MONGO_DATABASE ="meditha";
 	public final String MONGO_METADATA_COLLECTION ="metadata";
+	public final String MONGO_AUTHOR_COLLECTION ="author";
 	
 }
